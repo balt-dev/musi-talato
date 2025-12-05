7a4cc36ba49b5cfbadf48d6b7a5d57f313ab40be
balt-dev <heptor42@gmail.com>
2025-12-05 12:39:55 -0600

    Add Galdur support

---
+++ localization/crossmod/galdur.lua
@@ -0,0 +1,55 @@
+return {
+	misc = {
+		dictionary = {
+			gald_master = "wile Galdur",
+            gald_use_desc = {
+                "ante ni li weka e",
+                "pali pi namako Galdur"    
+            },
+            gald_anim = "wile tawa pona",
+            gald_anim_desc = {
+                "ante ni li ante e",
+                "tawa pi utala en kulupu"    
+            },
+            gald_reduce = "kulupu lili",
+            gald_reduce_desc = {
+                "ante ni li lili",
+                "e lipu lon tan sitelen"    
+            },
+            gald_stake_colour = "utala ko",
+            gald_stake_colour_options = {
+                "o ko e pini ala",    
+                "o ko e pini",
+            },
+            gald_stake_colour_desc = {
+                "o ante e utala",
+                "pi ko tan kipisi utala"
+            },
+            gald_stake_select = "pali utala",
+            gald_stake_select_options = {
+                "pali pini",    
+                "pali sin",
+                "utala walo"
+            },
+            gald_stake_select_desc = {
+                "ante ni li pali",
+                "e utala pi kipisi open"
+            },
+            gald_unlock = "ken pali e utala ale",
+            gald_unlock_desc = {
+                "ante ni la utala ale li ken pali"    
+            },
+            gald_select_deck = "pali nasin",
+            gald_select_stake = "pali utala",
+    	    gald_random_deck = "nasin ante",
+    	    gald_random_stake = "utala ante",
+            gald_play = "pali",
+            gald_selected = "lon",
+            gald_locked = "ken ala",
+            gald_unlock_1 = "pini kepeken nasin ni tan utala",
+            gald_unlock_and = " en ",
+            gald_new_page_error = "pakala tan open kipisi:",
+            gald_quick_start = "musi pini:"
+        }
+    }
+}
